6bd46ebf1eb6daf03d8f12d65516cc68eb3c76d3
Kajsa Veschetti <46371230+KajsaVeschetti@users.noreply.github.com>
2020-11-26 13:15:22 +0100

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,48 @@
 import React, {Component} from "react";
+import Habitato1 from "../img/Habitato1.jpg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 class Header extends Component {
 
+
   render(){
 
+    const headerStyle={
+      margin: "50px"
+    }
+
+    const logoStyle={
+      width: "200px",
+      height: "120px;",
+    }
+    const linkStyle = {
+      fontFamily: "Roboto",
+      fontSize: "20px",
+      color:"#5f5f5f",
+      textDecoration:"none",
+      display: "inline",
+      paddingRight:"80px",
+      paddingLeft:"80px",
+      
+    };
+
     return(
-      <div>
-        
-      </div>
+      <header style={headerStyle}>
+        <Row> 
+          <Col>
+          <a href="#" style={linkStyle}>Hem</a>
+          <a href="#" style={linkStyle}>Quiz</a>
+          <a href="index.html" >
+          <img src={Habitato1} style={logoStyle}  alt="Habitato logotyp"/>
+          </a>
+          <a href="#" style={linkStyle}>Tips</a>
+          <a href="#" style={linkStyle}>Fakt</a>
+          </Col>
+        </Row>
+       
+      </header>
     );
 
   }
