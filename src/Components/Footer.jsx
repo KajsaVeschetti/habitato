304d41372ea42d0996a7defa31da5562d3c96f0a
Kajsa Veschetti <46371230+KajsaVeschetti@users.noreply.github.com>
2020-11-26 15:43:40 +0100

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,13 +1,62 @@
 import React, {Component} from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Instagram from "../img/instagram.png";
+import Facebook from "../img/facebook.png";
+
 
 class Footer extends Component {
 
   render(){
 
+    const footerStyling ={
+      marginTop:"40px",
+      marginBottom:"40px",
+
+    }
+
+    const linkStyle = {
+      fontFamily: "Roboto",
+      fontSize: "14px",
+      color:"#5f5f5f",
+      textDecoration:"none",
+      listStyle: "none",
+      textAlign: "left",
+      paddingLeft: "45px"
+    };
+
+    const logos = {
+      width: "50px",
+      heigth: "50px",
+      margin: "5px"
+    }
+
+    const logoPlace = {
+      textAlign: "right",
+      paddingRight: "80px"
+    }
+
     return(
-      <div>
-        
-      </div>
+     <footer style={footerStyling}>
+       <Row>
+         <Col>
+         <ul style={linkStyle}>
+          <li><a href="#" style={linkStyle}>Hem</a></li>
+          <li><a href="#" style={linkStyle}>Quiz</a></li>
+          <li><a href="#" style={linkStyle}>Om Oss</a></li>
+          <li><a href="#" style={linkStyle}>Kontakta oss</a></li>
+          </ul>
+         </Col>
+         <Col>
+         <div style={logoPlace}>
+         <img src={Instagram} style={logos} alt="Instagram"/>
+         <img src={Facebook} style={logos} alt="Facebook"/>
+         </div>
+         </Col>
+       </Row>
+
+     </footer>
     );
 
   }
