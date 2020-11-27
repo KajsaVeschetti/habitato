3019c540c9f18bf37bbd56ab4d18ebc3b218c38e
Jenniehwest <50371951+Jenniehwest@users.noreply.github.com>
2020-11-27 15:20:33 +0100

    Create Facit.jsx

---
+++ src/Components/Facit.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class Facit extends Component {
+
+  render(){
+
+    return(
+      <div>
+        <p>FACIT KOMPONENT</p>
+      </div>
+    );
+
+  }
+}
+
+export default Facit;
