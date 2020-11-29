6abb40aa44e2986c2b4e75dec878f585bc3cc0e5
Kajsa Veschetti <46371230+KajsaVeschetti@users.noreply.github.com>
2020-11-29 02:30:45 +0100

    Create Tips.jsx

---
+++ src/Components/Tips.jsx
@@ -0,0 +1,14 @@
+import React, { Component, createContext } from 'react';
+
+class Tips extends Component {
+    state = {  }
+    render() { 
+
+
+        return ( 
+            <h1>Hello här testar vi saker</h1>
+         );
+    }
+}
+ 
+export default Tips;
