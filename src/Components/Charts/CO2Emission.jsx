3dfa9e9090e49ffdb0b59e81aaf737dd0bc5c414
Kajsa Veschetti <46371230+KajsaVeschetti@users.noreply.github.com>
2020-11-30 13:03:53 +0100

    Update CO2Emission.jsx

--- src/Components/Charts/CO2Emission.jsx
+++ src/Components/Charts/CO2Emission.jsx
@@ -7,13 +7,13 @@ import { BarChart, Bar, Cell,
   } from "recharts";
 
 
-class BarCharts extends Component {
+export default class BarCharts extends Component {
     handleYear = e => {
         if(e.key === "Enter")
-        {
-            this.props.onYearFilter(this.props.chartName, e.target.value);
-        }
-    };
+         {
+           this.props.onYearFilter(this.props.chartName, e.target.value);
+         }
+      };
 
     render() { 
 
@@ -22,36 +22,40 @@ class BarCharts extends Component {
         if(co2 === undefined) return <p>Ingen data finns</p>;
         
         const dataBarChart = [
-            {name: 'Gas', CO2: parseInt(co2["Gas Fuel"])},
-            {name: 'Liq', CO2: (co2["Liquid Fuel"])},
-            {name: 'Sol', CO2: (co2["Solid Fuel"])},
-            {name: 'Cem', CO2: (co2["Cement"])},
-        ];
+            {name: 'Gas ', CO2: parseInt(co2["Gas Fuel"])}, 
+            {name: 'Liq', CO2: co2["Liquid Fuel"] },
+            {name: 'Sol', CO2: co2["Solid Fuel"]}, 
+            {name: 'Cem', CO2: co2["Cement"]}
+          ];
 
         const COLORS =[ "#d9534f", "#5bc0de", "#5cb85c", "#428bca" ]
 
 
         return (  
-            <div style={{width:"350px", height:"400px"}}>
+            <div style={{width:"350px", height:"350px"}}>
                 <input type="text" placeholder="Välj ett år" onKeyDown={e=> this.handleYear(e)}></input>
                 <BarChart
-                width={350}
-                height={400}
-                data={dataBarChart}
-                margin={{top:5, right: 30, left: 20, bottom: 5, }}>
-                    <CartesianGrid storokeDasharray="3 3"/>
-                    <XAxis dataKey="name"/>
-                    <YAxis/>
-                    <Bar dataKey="CO2" fill="#8884d8">
-                        <Cell fill={COLORS[0]}/>
-                        <Cell fill={COLORS[1]}/>
-                        <Cell fill={COLORS[2]}/>
-                        <Cell fill={COLORS[3]}/>
-                    </Bar>
-                </BarChart>
+                    width={350}
+                    height={300}
+                    data={dataBarChart}
+                    margin={{
+                    top: 5, right: 30, left: 20, bottom: 5,
+                    }}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Bar dataKey="CO2" fill="#8884d8"> 
+                <Cell fill ={COLORS[0]}/>
+                <Cell fill ={COLORS[1]}/>
+                <Cell fill ={COLORS[2]}/>
+                <Cell fill ={COLORS[3]}/>
+                 <Cell fill ={COLORS[4]}/>
+
+        </Bar>
+       
+      </BarChart>
             </div>
         );
     }
 }
  
-export default BarCharts;
