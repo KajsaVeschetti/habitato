6c0b9d605008a002b33272728587e173630b6f60
Lina <46371492+linahoggren@users.noreply.github.com>
2020-12-01 10:43:21 +0000

    Update Comments.jsx

--- src/Components/Comments.jsx
+++ src/Components/Comments.jsx
@@ -1,22 +1,12 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 // class Comments extends Component {
 
-  
-
-  
-  
-  
-  
-  
-  
 //   render(){
 
 //     return(
 //       <div>
 //         <p>hej</p>
-
-   
 
 //       </div>
 //     );
@@ -24,45 +14,49 @@ import React, {Component} from "react";
 //   }
 // }
 
-
 class Comments extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = { value: "" };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {    this.setState({value: event.target.value});  }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+    alert("A name was submitted: " + this.state.value);
     event.preventDefault();
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Namn:
-          <br/>
-          <input type="text" value={this.state.value} onChange={this.handleChange} />        
-          </label> <br/>
+      <div>
+        <h2>Dela med dig av ditt bästa miljötips!</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Namn:
+            <br />
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>{" "}
+          <br />
           <label>
             Kommentar:
-            <br/>
+            <br />
             <textarea value={this.state.value} onChange={this.handleChange} />
-          </label><br/>
-        <input type="submit" value="Skicka kommentar" />
-      </form>
+          </label>
+          <br />
+          <input type="submit" value="Skicka kommentar" />
+        </form>
+      </div>
     );
   }
 }
-
-
-
-
-
-
 
 export default Comments;
 
