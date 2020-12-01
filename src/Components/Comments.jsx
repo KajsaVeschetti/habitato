4b336aa14a1a2677da88b0c493a9e989accb9a72
Lina <46371492+linahoggren@users.noreply.github.com>
2020-12-01 10:41:06 +0000

    Update Comments.jsx

--- src/Components/Comments.jsx
+++ src/Components/Comments.jsx
@@ -1,16 +1,70 @@
 import React, {Component} from "react";
 
+// class Comments extends Component {
+
+  
+
+  
+  
+  
+  
+  
+  
+//   render(){
+
+//     return(
+//       <div>
+//         <p>hej</p>
+
+   
+
+//       </div>
+//     );
+
+//   }
+// }
+
+
 class Comments extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  render(){
+  handleChange(event) {    this.setState({value: event.target.value});  }
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
 
-    return(
-      <div>
-        <p> hej</p>
-      </div>
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Namn:
+          <br/>
+          <input type="text" value={this.state.value} onChange={this.handleChange} />        
+          </label> <br/>
+          <label>
+            Kommentar:
+            <br/>
+            <textarea value={this.state.value} onChange={this.handleChange} />
+          </label><br/>
+        <input type="submit" value="Skicka kommentar" />
+      </form>
     );
-
   }
 }
 
+
+
+
+
+
+
 export default Comments;
+
+
+// kodinspiration från https://reactjs.org/docs/forms.html
