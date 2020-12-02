c516e9cbdfd2229680fdfb89cba99e4566f98107
Jenniehwest <50371951+Jenniehwest@users.noreply.github.com>
2020-12-02 13:27:16 +0100

    Update Tips.jsx

--- src/Components/Tips.jsx
+++ src/Components/Tips.jsx
@@ -1,14 +1,39 @@
-import React, { Component, createContext } from 'react';
+import React, { Component } from 'react';
+import Quiz from './Quiz';
+import Facit from './Facit';
 
-class Tips extends Component {
-    state = {  }
-    render() { 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
+export default function App() {
+  return (
+    <Router>
+      <div>
+      <h1>TIPS KOMPONENT</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/quiz">Vill du göra quizet igen?</Link>
+            </li>
+            <li>
+              <Link to="/facit">eller vill du läsa om de rätta svaren?</Link>
+            </li>
+          </ul>
+        </nav>
 
-        return ( 
-            <h1>Hello här testar vi saker</h1>
-         );
-    }
+        <Switch>
+          <Route path="/quiz">
+            <Quiz />
+          </Route>
+          <Route path="/facit">
+            <Facit />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
- 
-export default Tips;
