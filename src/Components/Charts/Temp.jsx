9bddbcf8ddd46bfb7ddcb38a906377c7e49e91f0
Kajsa Veschetti <46371230+KajsaVeschetti@users.noreply.github.com>
2020-12-02 10:21:14 +0100

    Create Temp.jsx

---
+++ src/Components/Charts/Temp.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    Label
+  } from "recharts";
+class Temp extends Component {
+    
+
+
+    render() { 
+
+        return ( 
+            <React.Fragment>
+                
+            <div style={{width:"350px", height:"400px", float:"left"}}>
+                {/* <input type="text" placeholder="Välj ett år"></input> */}
+                <LineChart
+          width={350}
+          height={300}
+          data={this.props.Tempdata}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="Year" />
+          <YAxis />
+          <Tooltip />
+          <Legend verticalAlign="top" />
+          <Line
+            type="monotone"
+            dataKey="Mean"
+            stroke="#8884d8"
+            activeDot={{ r: 12 }}
+          />
+        </LineChart>
+            </div>
+            </React.Fragment>
+         );
+    }
+}
+ 
+export default Temp;
