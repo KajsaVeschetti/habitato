fd99f4e859b5ba45b6578689b9afb7d2e21957cf
Lina <46371492+linahoggren@users.noreply.github.com>
2020-12-02 19:42:59 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-<<<<<<< Updated upstream
-
-import Page from './Components/Page';
-=======
 // import Page from "./Components/Page";
 import NavBar from './Components/NavBar';
 import Quiz from './Components/Quiz';
 import Footer from './Components/Footer';
 import AllCharts from './Components/Charts/AllCharts';
 import Tips from './Components/Tips';
-import Test from './Components/Test';
 import CommentGroup from './Components/CommentGroup';
 import FaktaRutor from './Components/FaktaRutor';
->>>>>>> Stashed changes
 
 
 
@@ -24,20 +18,14 @@ class App extends Component{
   render(){
     return (
       <div className="App">
-<<<<<<< Updated upstream
-
-
-      <Page/>
-=======
         <NavBar/>
        <Tips/>
-       <Test/>
+       <CommentGroup />
       <AllCharts/>
-      <CommentGroup />
+      
       <FaktaRutor/>
       <Quiz/>
       <Footer/>
->>>>>>> Stashed changes
 
       </div>
     );
