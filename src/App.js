ab1393597f0389c7bfe30ad83b15eb1ebe40402b
Kajsa Veschetti <46371230+KajsaVeschetti@users.noreply.github.com>
2020-12-02 15:54:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-// import Page from "./Components/Page";
-import NavBar from './Components/NavBar';
-import Quiz from './Components/Quiz';
-import Footer from './Components/Footer';
-import AllCharts from './Components/Charts/AllCharts';
-import Tips from './Components/Tips';
-import Comments from './Components/Comments';
-import FaktaRutor from './Components/FaktaRutor';
+
 import Page from './Components/Page';
 
 
@@ -19,13 +12,7 @@ class App extends Component{
   render(){
     return (
       <div className="App">
-        {/* <NavBar/>
-       <Tips/>
-      {/* <AllCharts/>
-      <Comments />
-      <FaktaRutor/>
-      <Quiz/>
-      <Footer/> */}
+
 
       <Page/>
 
