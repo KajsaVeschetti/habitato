08f62f8ec4691bab79e62b5a22950b0424a7b4e6
Kajsa Veschetti <46371230+KajsaVeschetti@users.noreply.github.com>
2020-12-02 13:42:44 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 // import Page from "./Components/Page";
-import NavBar from './Components/NavBar';
-import Quiz from './Components/Quiz';
-import Footer from './Components/Footer';
-import AllCharts from './Components/Charts/AllCharts';
-import Tips from './Components/Tips';
-import Test from './Components/Test';
-import Comments from './Components/Comments';
-import FaktaRutor from './Components/FaktaRutor';
+
 import Page from './Components/Page';
 
 
