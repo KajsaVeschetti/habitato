28137bc79fb635505f77987e9126240b6d112337
Annica113 <45743272+Annica113@users.noreply.github.com>
2020-12-03 02:37:51 +0100

    Update FaktaRutor.jsx

--- src/Components/FaktaRutor.jsx
+++ src/Components/FaktaRutor.jsx
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
-import {Button, Collapse, Image} from 'react-bootstrap';
+import {Button, Collapse, Image, Container, Row, Col} from 'react-bootstrap';
 import Fuel from '../img/Fuel.jpg';
-;
-
-
 
 
 function Fakta() {
@@ -11,9 +8,9 @@ function Fakta() {
 
   return ( 
     <>
-    <div className="container">
-    <div className="row">
-    <div className="col-md-6 p-5 col-xs-6">
+    <Container>
+    <Row>
+   <Col className="p-4">
      <h3 className="text-center p-3">Vad är fossila bränslen?</h3>
       <p>Dagens fossila bränslen skapades för miljontals år sedan av resterna från döda djur och växter. Resterna bröts ned och ombildades till exempelvis kol, naturgas och olja. Ur dessa fossila bränslen utvinner vi energi som t ex används till att driva motorer, uppvärmning och för att skapa elektricitet.</p>
 
@@ -33,14 +30,14 @@ function Fakta() {
         <p>Fossila bränslen leder inte bara till klimatförändringar utan även till miljöförstöring och negativa hälsoeffekter. Vår förbrukning av fossila bränslen sker dessutom i snabbare takt än återskapandet. Det är en icke-förnybar energikälla, som en dag kommer ta slut. Det är därför viktig att hitta hållbara alternativ. Världsnaturfonden menar att förnybar energi, såsom exempelvis solkraft, vattenkraft och vindkraft, är en del av lösningen. Det beror på att med förnybar energi slipper vi föroreningar och farliga restprodukter, det kan aldrig ta slut och påverkar inte naturen negativt.</p>
         </div>
       </Collapse>
-      </div>
+      </Col>
 
-      <div className="col-md-6 col-xs-6 mt-4 mb-5">
+      <Col className="mt-4 mb-5">
   <Image src={Fuel}fluid />
-  </div>
+  </Col>
 
-</div>
-</div>
+</Row>
+</Container>
 
  </>
 
