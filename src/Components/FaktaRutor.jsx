0f70c0373e225094e4bb01e8eebed8078f411f29
Annica113 <45743272+Annica113@users.noreply.github.com>
2020-12-03 07:13:19 +0100

    Update FaktaRutor.jsx

--- src/Components/FaktaRutor.jsx
+++ src/Components/FaktaRutor.jsx
@@ -10,7 +10,7 @@ function Fakta() {
     <>
     <Container>
     <Row>
-   <Col className="p-4">
+   <div className="col-md-6 p-5 col-xs-6 p-4">
      <h3 className="text-center p-4">Vad är fossila bränslen?</h3>
       <p>Dagens fossila bränslen skapades för miljontals år sedan av resterna från döda djur och växter. Resterna bröts ned och ombildades till exempelvis kol, naturgas och olja. Ur dessa fossila bränslen utvinner vi energi som t ex används till att driva motorer, uppvärmning och för att skapa elektricitet.</p>
 
@@ -30,11 +30,13 @@ function Fakta() {
         <p>Fossila bränslen leder inte bara till klimatförändringar utan även till miljöförstöring och negativa hälsoeffekter. Vår förbrukning av fossila bränslen sker dessutom i snabbare takt än återskapandet. Det är en icke-förnybar energikälla, som en dag kommer ta slut. Det är därför viktig att hitta hållbara alternativ. Världsnaturfonden menar att förnybar energi, såsom exempelvis solkraft, vattenkraft och vindkraft, är en del av lösningen. Det beror på att med förnybar energi slipper vi föroreningar och farliga restprodukter, det kan aldrig ta slut och påverkar inte naturen negativt.</p>
         </div>
       </Collapse>
-      </Col>
+      </div>
+      
+  
+     <div className="col-md-6 col-xs-6 mt-4 mb-5">
+    <Image src={Fuel}fluid rounded/>
+    </div>
 
-      <Col className="mt-4 mb-5">
-  <Image src={Fuel}fluid />
-  </Col>
 
 </Row>
 </Container>
