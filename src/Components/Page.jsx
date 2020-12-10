4d24335671f7aaf58d4ce3a6017073d4c78cde2d
Kajsa Veschetti <46371230+KajsaVeschetti@users.noreply.github.com>
2020-12-10 15:42:04 +0100

    Update Page.jsx

--- src/Components/Page.jsx
+++ src/Components/Page.jsx
@@ -2,10 +2,6 @@ import React, {Component} from "react";
 // import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar"
 import Footer from "./Footer";
-import AllCharts from "./Charts/AllCharts";
-import { faKickstarter } from "@fortawesome/free-brands-svg-icons";
-import Fakta from "./FaktaRutor";
-import CommentGroup from "./CommentGroup";
 
 
 
@@ -14,11 +10,10 @@ class Page extends Component {
 
   render(){
     return(
-     <>
+     <div className="wrapper">
         <NavBar/>
-        <div className= "Container"></div>
         <Footer/>
-      </>
+      </div>
     );
 
   }
