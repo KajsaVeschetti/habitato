62a898aa4773cad3fbab5d97178842aaedde92d4
Annica113 <45743272+Annica113@users.noreply.github.com>
2020-12-14 08:37:01 +0100

    Update FaktaRutor.jsx

--- src/Components/FaktaRutor.jsx
+++ src/Components/FaktaRutor.jsx
@@ -1,50 +1,80 @@
-import React, { useState } from 'react';
-import {Button, Collapse, Image, Container, Row, Col} from 'react-bootstrap';
-import Fuel from '../img/Fuel.jpg';
+import React, { Component } from 'react';
+import FossilFuel from './Fakta/FossilFuel'; 
+import GlobalWarming from './Fakta/GlobalWarming'; 
+import GlacierSize from './Fakta/GlacierSize'; 
+import SeaLevel from './Fakta/SeaLevel'; 
+import {Container, Card, Nav, Navbar} from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom'; 
 
+class Fakta extends Component {
 
-function Fakta() {
-  let [open, setOpen] = useState(false);
-
-  return ( 
-    <>
-    <Container>
-    <Row>
-   <div className="col-md-6 p-5 col-xs-6 p-3">
-     <h3 className="text-center p-3">Vad är fossila bränslen?</h3>
-      <p>Dagens fossila bränslen skapades för miljontals år sedan av resterna från döda djur och växter. Resterna bröts ned och ombildades till exempelvis kol, naturgas och olja. Ur dessa fossila bränslen utvinner vi energi som t ex används till att driva motorer, uppvärmning och för att skapa elektricitet.</p>
-
-      <Button variant="secondary" size="sm" block
-        onClick ={() => setOpen(!open)}
-        aria-controls="fuel-text"
-        aria-expanded={open}
-        >
-        Läs mer
-      </Button>
-
-      <Collapse in={open}>
-        <div id="fuel-text">
-        <h4 className="text-center mt-5">Varför är fossila bränslen dåligt för klimatet?</h4>
-          <p>Vid förbränning av fossila bränslen släpps det ut farliga gaser såsom exempelvis kväveoxider och svaveloxider, och den totala koldioxidhalten i atmosfären ökar. Förbränning av fossila bränslen bidrar till att förstärka växthuseffekten vilket gör att jordens värmebalans rubbas. Det får negativa konsekvenser för klimat och ekosystem.</p>.
-        <h4 className="text-center">Vad kan vi göra?</h4>
-        <p>Fossila bränslen leder inte bara till klimatförändringar utan även till miljöförstöring och negativa hälsoeffekter. Vår förbrukning av fossila bränslen sker dessutom i snabbare takt än återskapandet. Det är en icke-förnybar energikälla, som en dag kommer ta slut. Det är därför viktig att hitta hållbara alternativ. Världsnaturfonden menar att förnybar energi, såsom exempelvis solkraft, vattenkraft och vindkraft, är en del av lösningen. Det beror på att med förnybar energi slipper vi föroreningar och farliga restprodukter, det kan aldrig ta slut och påverkar inte naturen negativt.</p>
-        </div>
-      </Collapse>
-      </div>
-      
-  
-     <div className="col-md-6 col-xs-6 mt-4 mb-5">
-    <Image src={Fuel}fluid rounded/>
-    </div>
-
-
-</Row>
-</Container>
-
- </>
-
-
-);
+render() {
+const linkStyle={
+fontSize: "16px",
+fontFamily: "Quicksand",
+color:"#5f5f5f",
+textDecoration:"none",
+paddingLeft: "15px",
+paddingRight: "15px",
 }
 
+ return(
+   <>
+<Container className="col-xl-6 fluid=sm text-center mb-5 mt-4 bg-white">
+  <h3 className="p-4">Fakta om klimatförändringar</h3>
+<Card border="light"className="bg-light pl-4 pr-4 pb-4">
+    <Card.Body>  
+<Router>
+<Navbar collapseOnSelect expand="xl" bg="light" variant="light">
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mx-auto font-weight-normal">
+<Link class="nav-link" style={linkStyle} to="/FossilFuel">
+Fossilt bränsle  
+</Link>
+<Link class="nav-link " style={linkStyle} to="/GlobalWarming" >
+Global uppvärmning 
+</Link>
+<Link class="nav-link " style={linkStyle} to="/GlacierSize"> 
+Glaciär storlek 
+</Link>
+<Link class="nav-link " style={linkStyle} to="/SeaLevel">
+Havsnivå 
+</Link>
+</Nav>
+  </Navbar.Collapse>
+</Navbar>  
+
+<Switch>
+  <div class="mx-auto">
+              <Route exact path='/FossilFuel' component={FossilFuel}>
+              <FossilFuel/>
+              </Route> 
+            
+              <Route exact path='/GlobalWarming' component={GlobalWarming}>
+              <GlobalWarming/>
+              </Route> 
+                
+              <Route exact path='/GlacierSize' component={GlacierSize}>
+              <GlacierSize/>
+              </Route> 
+               
+              <Route exact path='/SeaLevel' component={SeaLevel}>
+              <SeaLevel/>
+              </Route> 
+             <Redirect from="/" exact to="/FossilFuel"></Redirect>
+          </div>
+          
+      
+           </Switch>
+ 
+       </Router> 
+    </Card.Body>
+    </Card>
+       </Container>
+       
+</>
+ );
+ }
+}
 export default Fakta;
