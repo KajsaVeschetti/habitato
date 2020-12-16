3f876294599e723f20a170bc802fa42d2b246da9
Kajsa Veschetti <46371230+KajsaVeschetti@users.noreply.github.com>
2020-12-16 15:06:13 +0100

    Create ReadMore.jsx

---
+++ src/Components/Charts/InfoCharts/ReadMore.jsx
@@ -0,0 +1,53 @@
+import React,{useState} from 'react';
+import { Button } from 'react-bootstrap';
+
+
+const LinkStyle = {
+    fontFamily: "Quicksand",
+    color:"#1F1F1F",
+    paddingRight:"150px",
+    paddingLeft:"150px",
+
+  }
+
+  
+  const extra ={
+    width: "600px"
+  }
+
+
+  const BtnStyle = {
+    fontFamily: "Quicksand",
+    fontSize: "15px",
+    color:"white",
+    paddingRight:"150px",
+    paddingLeft:"150px",
+
+  }
+  
+  
+function ReadMore() {
+  const [readMore,setReadMore]=useState(false);
+
+
+  const extraContent=<div style={extra}>
+      <p className="extra-content" >
+
+Co2 är den kemiska formeln för Koldioxid. Koldioxid är en färg- och luktfri gas. 
+Runt jorden, i vår atmosfär, finns det flera olika gaser, bland annat koldioxid. Men även metan, lustgas och andra så kallade växthusgaser. Växthusgaserna hjälper till att hålla jorden varm, utan den naturliga växthuseffekten skulle vår jord vara ungefär 30 grader kallare än vad den är idag.
+
+När växthusgaserna ökar i atmosfären förstärks den naturliga växthuseffekten, vilket leder till att jordens medeltemperatur ökar. Det är den här temperaturökningen som kallas för global uppvärmning.
+      </p>
+  </div>
+  const BtnName=readMore?'Göm info ':'Läs mer om Co2 '
+  return (
+    <div className="App">
+
+      <Button style={BtnStyle}  size="sm" variant="info" className="read-more-link" onClick={()=>{setReadMore(!readMore)}}>{BtnName}</Button>
+
+      {readMore && extraContent}
+    </div>
+  );
+}
+
+export default ReadMore;
