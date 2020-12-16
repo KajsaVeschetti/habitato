a170fb1d3a785fa9c7bf86bd07011862e3776ba7
Jenniehwest <50371951+Jenniehwest@users.noreply.github.com>
2020-12-16 15:31:41 +0100

    Update ReadMoreSL.jsx

--- src/Components/Charts/InfoCharts/ReadMoreSL.jsx
+++ src/Components/Charts/InfoCharts/ReadMoreSL.jsx
@@ -1,30 +1,42 @@
 import React,{useState} from 'react';
+import { Button } from 'react-bootstrap';
+
 
 const LinkStyle = {
     fontFamily: "Quicksand",
     color:"#1F1F1F",
     paddingRight:"150px",
     paddingLeft:"150px",
-
   }
 
-  const extra ={
-    width: "600px"
+  const extra = {
+    width: "600px",
+    fontFamily: "Quicksand",
   }
 
-
+  const BtnStyle = {
+    fontFamily: "Quicksand",
+    fontSize: "15px",
+    color:"white",
+    paddingRight:"150px",
+    paddingLeft:"150px",
+  }
+  
 function ReadMore() {
   const [readMore,setReadMore]=useState(false);
-  const extraContent=<div style={extra}>
-      <p className="extra-content">
-      Den globala uppvärmningen gör så att vattnet på jorden blir varmare och när vatten värms utvidgas det. Den höjda temperaturen leder också till att glaciärer och polarisar smälter. Dessa två kombinationerna gör så att havsnivån höjs.
-Följder av en höjd havsnivå kan till exempel vara översvämningar och att stora delar av kustområden försvinner. Havsnivån mäts en gång om året, den 15 Mars.</p>
+
+
+  const extraContent=
+    <div style={extra}>
+      <p>Den globala uppvärmningen gör så att vattnet på jorden blir varmare och när vatten värms utvidgas det. Den höjda temperaturen leder också till att glaciärer och polarisar smälter. Dessa två kombinationerna gör så att havsnivån höjs.Följder av en höjd havsnivå kan till exempel vara översvämningar och att stora delar av kustområden försvinner.</p>
   </div>
-  const linkName=readMore?'Göm info ':'Läs mer om Havsnivå '
+  
+  const BtnName=
+  readMore?'Göm info ':'Läs mer om havsnivå '
+  
   return (
     <div className="App">
-      <a style={LinkStyle} className="read-more-link" onClick={()=>{setReadMore(!readMore)}}><h2>{linkName}</h2></a>
-      {readMore && extraContent}
+      <Button style={BtnStyle}  size="sm" variant="info"  onClick={()=>{setReadMore(!readMore)}}>{BtnName}</Button> {readMore && extraContent}
     </div>
   );
 }
