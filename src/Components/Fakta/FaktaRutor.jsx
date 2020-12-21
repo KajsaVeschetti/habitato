26e983c5ccd25b67138568bb7f507e3ed75eff52
Annica113 <45743272+Annica113@users.noreply.github.com>
2020-12-21 17:52:03 +0100

    Update FaktaRutor.jsx

--- src/Components/Fakta/FaktaRutor.jsx
+++ src/Components/Fakta/FaktaRutor.jsx
@@ -18,7 +18,7 @@ class Fakta extends Component {
 
    return(
    <>
-   <Container id="test" className="col-lg-9 col-xs-12 px-lg-5 px-xs-0 pb-5 rounded">
+   <Container id="test" className="col-lg-9 col-xs-12 px-lg-5 px-xs-2 pb-5 rounded">
     <h3 className="pt-5 pb-2">Fakta om klimatförändringar</h3> 
     <Card className="bg-light rounded">
    <Card.Body>
