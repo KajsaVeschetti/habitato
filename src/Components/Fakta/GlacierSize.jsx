450b2012e6a983e7afa0dc9361c52f65cf3fc52f
Annica113 <45743272+Annica113@users.noreply.github.com>
2020-12-21 16:15:03 +0100

    Update GlacierSize.jsx

--- src/Components/Fakta/GlacierSize.jsx
+++ src/Components/Fakta/GlacierSize.jsx
@@ -3,9 +3,9 @@ import {Button, Collapse, Image, Card} from 'react-bootstrap';
 import glacierSize from "./faktaImg/glacierSize.jpg";
 
 function GlacierSize () {
-  let [open, setOpen] = useState(false);
+  let [openButton, setOpenButton] = useState(false);
 
-  const buttonName=open?'Läs mindre ':'Läs mer '
+  const buttonName=openButton?'Läs mindre ':'Läs mer '
 
   return ( 
     <>
@@ -15,7 +15,7 @@ function GlacierSize () {
      <Card.Title>Varför minskar glaciärerna i storlek?</Card.Title>
       <Card.Text className="pb-3 text-left">Ungefär 10 procent av jordens yta täcks av glaciärer, men de flesta återfinns i Antarktis och på Grönland. Glaciärerna kan betraktas som världens största sötvattensreserv, men i takt med att jordens medeltemperatur ökar så har glaciärerna börjat smälta i allt snabbare takt; under sommaren smälter mer av isen och mindre återbildas under vintern. Det gör att glaciärernas storlek minskar. Denna process går allt snabbare och många forskare oroar sig för följderna då jordens kylskåp inte längre är tillräckligt kallt, och all is slutligen har smält. </Card.Text>
 
-      <Collapse in={open}> 
+      <Collapse in={openButton}> 
         <div id="glacier-collapse-text">
         <Card.Title className="pt-2">Vilka konsekvenserna får smältande glaciärer?</Card.Title>
         <Card.Text className="text-left"> De två främsta konsekvenserna av att glaciärisen smälter är dels att havsnivåerna höjs, dels att färskvattnet minskar. Det gör oss mer sårbara eftersom tillgången till vatten och ökade havsnivåer påverkar allt liv på jorden. Om alla glaciärer smälte skulle havsnivån stiga med över 120 meter. Det skulle drabba många kuststäder och hela länder som då hamnar under havsytan, t ex New York och Bangladesh. Många platser på jorden är också beroende av färskvattnet från glaciärer, bland annat länderna runt Himalaya och Anderna.</Card.Text>.
@@ -28,9 +28,9 @@ function GlacierSize () {
       </Collapse>
 
       <Button variant="info" size="sm" block    
-      onClick={() => setOpen(!open)}
+      onClick={() => setOpenButton(!openButton)}
       aria-controls="glacier-collapse-text"
-      aria-expanded={open}
+      aria-expanded={openButton}
      >{buttonName}
      </Button>
   
