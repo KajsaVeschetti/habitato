d4888c1548cd522acf3f1b2cdc4f7745da5c86af
Jenniehwest <50371951+Jenniehwest@users.noreply.github.com>
2020-12-22 13:31:44 +0100

    Update QBtns.jsx

--- src/Components/Quiz/QBtns.jsx
+++ src/Components/Quiz/QBtns.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
-import { Button, Container } from 'react-bootstrap';
+import { Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-
 const QBtns = ({ question, options, selected }) => {
-    const [answer, setAnswer] = useState(options);
+    // state kan bara användas i class. UseState returns en array inte objekt
+    const [answer, setAnswer] = useState(options); //här gör det så att man kan använda statevariabeln i en funktionen.
     return (
         <div>
             <div>{question}</div>
                 <div class="questWrap">
+                    {/* mappar ut knappar */}
                 {answer.map((text) => (
                     <Button class="btnWrap" variant="info" size="sm" onClick={() => {
                         setAnswer([text]);
