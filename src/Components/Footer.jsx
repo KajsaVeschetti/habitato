20091a2f06beffe8b43c3330f6a52918ce50ca15
Kajsa Veschetti <46371230+KajsaVeschetti@users.noreply.github.com>
2020-12-23 01:02:36 +0100

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,68 +10,24 @@ class Footer extends Component {
 
 
   render(){ 
-    
-
-    const linkStyle = {
-      fontSize: "14px",
-      color:"#5f5f5f",
-      textDecoration:"none",
-     display:"inline",
-     margin:"20px"
-    };
-
-    const listStyle = {
-      fontSize: "14px",
-      color:"#5f5f5f",
-      textDecoration:"none",
-      listStyle:"none",
-      float: "center",
-      display:"inline",
-      listStyleType:"none",
-    };
-
-    const logos = {
-      margin: "5px",
-      color: "#5f5f5f",
-    }
-
-  
-    const onMouseOver = event => {
-      const el = event.target;
-      let colorhex = "#5cb85c";
-      el.style.color = colorhex;
-    };
-    
-    const onMouseOut = event => {
-      const el = event.target;
-      let grey = "#5f5f5f";
-      el.style.color = grey;
-    };
-    
-
-
     return(
            
         <footer className="fixedFooter">
      
-         <ul class="footerMenu" style={listStyle}>
-          <li><a href="/" style={linkStyle}>Hem</a></li>
-          <li><a href="/Quiz" style={linkStyle}>Quiz</a></li>
-          <li><a href="/Kontakt" style={linkStyle}>Kontakta oss</a></li>
+         <ul class="footerMenu" className="footerStyle" >
+          <li><a href="/" className="footerLinks" >Hem</a></li>
+          <li><a href="/Quiz"  className="footerLinks" >Quiz</a></li>
+          <li><a href="/Kontakt"  className="footerLinks" >Kontakta oss</a></li>
           </ul>
           <div >
-         <a href="#" 
-              onMouseEnter={event => onMouseOver(event)}
-              onMouseLeave={event => onMouseOut(event)}>
-           <FontAwesomeIcon style={logos} size='lg' icon={faFacebookSquare}/></a>
-         <a href="#"
-             onMouseEnter={event => onMouseOver(event)}
-             onMouseLeave={event => onMouseOut(event)}
-          ><FontAwesomeIcon style={logos} size="lg"  icon={faInstagram}/></a>
+         <a href="https://www.facebook.com/" >
+
+           <FontAwesomeIcon className="footerLogo"  icon={faFacebookSquare}/></a>
+         <a href="https://www.instagram.com/">
+          <FontAwesomeIcon className="footerLogo"   icon={faInstagram}/></a>
          </div>
      </footer>
-    
-
+  
     );
 
   }
