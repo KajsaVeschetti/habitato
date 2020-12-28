cf82b8df463ff2a530f17e73235d7e1e7085ea22
Jenniehwest <50371951+Jenniehwest@users.noreply.github.com>
2020-12-28 15:10:46 +0100

    Update Resultat.jsx

--- src/Components/Quiz/Resultat.jsx
+++ src/Components/Quiz/Resultat.jsx
@@ -9,8 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Resultat = ({score, playAgain, responses }) => 
 <div class="contentWrap" >
     <div> 
-        {/* Funkar inte måste hårdkoda antal frågor */}
-        <h2>Du fick {score} av {responses}  10 möjliga </h2> 
+        <h2>Du fick {score} av {responses} möjliga </h2> 
     </div>
 
     <div class="btnWrap">
