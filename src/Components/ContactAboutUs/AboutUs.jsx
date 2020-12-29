8b28857159991d63253861bb79b5d62adba85a11
Lina <46371492+linahoggren@users.noreply.github.com>
2020-12-29 12:00:48 +0000

    Update AboutUs.jsx

--- src/Components/ContactAboutUs/AboutUs.jsx
+++ src/Components/ContactAboutUs/AboutUs.jsx
@@ -8,7 +8,111 @@ import ann from "./img/ann.svg";
 class AboutUs extends Component {
     render() {
         return (
-            // <div className="aboutUsContainer">
+           <Container >
+                <Row>
+                    <Col>
+                        <Card className="aboutCard cardKarl">
+                            <Card.Img 
+                                className="aboutUsImg" 
+                                variant="top" 
+                                src={karl}/>
+                            <Card.Body>
+                                <Card.Title>
+                                    Karl Larsson
+                                </Card.Title>
+                                <Card.Text>
+                                    Karl Larsson är professor i Fysik och Geovetenskap, att förstå jorden ligger honom varmt om hjärtat. 
+                                </Card.Text>
+                                <Button 
+                                    variant="info" 
+                                    href="mailto:#">
+                                        Kontakta Karl
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card className="aboutCard cardLars">
+                            <Card.Img 
+                                className="aboutUsImg" 
+                                variant="top" 
+                                src={lars}/>
+                            <Card.Body>
+                                <Card.Title>
+                                    Lars Karlsson
+                                </Card.Title>
+                                <Card.Text>
+                                    Lars Karlsson är professor i Biologi och Etologi och intresserar sig mycket i natur, blommor och bin.
+                                </Card.Text>
+                                <Button 
+                                    variant="info" 
+                                    href="mailto:#">
+                                        Kontakta Lars
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Card className=" aboutCard cardLinn">
+                            <Card.Img 
+                                className="aboutUsImg" 
+                                variant="top" 
+                                src={linn}/>
+                            <Card.Body>
+                                <Card.Title>
+                                    Linn Annson
+                                </Card.Title>
+                                <Card.Text>
+                                    Linn Annson är professor i Social-antropologi med intresse för samhället och kultur. 
+                                </Card.Text>
+                                <Button 
+                                    variant="info" 
+                                    href="mailto:#">
+                                        Kontakta Linn
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card className="aboutCard cardAnn">
+                            <Card.Img 
+                                className="aboutUsImg" 
+                                variant="top" 
+                                src={ann}/>
+                            <Card.Body>
+                                <Card.Title>
+                                    Ann Linnson
+                                </Card.Title>
+                                <Card.Text>
+                                    Ann Linnson är forskare inom Vulkanologi och brinner för att hitta lösningar för att bevara jorden. 
+                                </Card.Text>
+                                <Button 
+                                    variant="info" 
+                                    href="mailto:#">
+                                        Kontakta Ann
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
+}
+    
+export default AboutUs;
+
+
+
+
+
+
+// ******************* TEST 1 ******************** //
+
+
+// <div className="aboutUsContainer">
                 
             //     <Card className="cardKarl" style={{ width: '18rem' }}>
             //         <Card.Img className="aboutUsImg" variant="top" src={karl} />
@@ -52,66 +156,3 @@ class AboutUs extends Component {
             //     </Card>
                 
             // </div>
-
-
-            <Container >
-                <Row>
-                    <Col>
-                        <Card className="aboutCard cardKarl" >
-                            <Card.Img className="aboutUsImg" variant="top" src={karl} />
-                            <Card.Body>
-                                <Card.Title>Karl Larsson</Card.Title>
-                                <Card.Text>
-                                    Karl Larsson är professor i Fysik och Geovetenskap, att förstå jorden ligger honom varmt om hjärtat. 
-                                </Card.Text>
-                                <Button variant="info" href="mailto:#" >Kontakta Karl</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card className="aboutCard cardLars" >
-                            <Card.Img className="aboutUsImg" variant="top" src={lars} />
-                            <Card.Body>
-                                <Card.Title>Lars Karlsson</Card.Title>
-                                <Card.Text>
-                                    Lars Karlsson är professor i Biologi och Etologi och intresserar sig mycket i natur, blommor och bin.
-                                </Card.Text>
-                                <Button variant="info" href="mailto:#" >Kontakta Lars</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Card className=" aboutCard cardLinn" >
-                            <Card.Img className="aboutUsImg" variant="top" src={linn} />
-                            <Card.Body>
-                                <Card.Title>Linn Annson</Card.Title>
-                                <Card.Text className="aboutUsCardText">
-                                    Linn Annson är professor i Social-antropologi med intresse för samhället och kultur. 
-                                </Card.Text>
-                                <Button variant="info" href="mailto:#" >Kontakta Linn</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card className="aboutCard cardAnn" >
-                            <Card.Img className="aboutUsImg" variant="top" src={ann} />
-                            <Card.Body>
-                                <Card.Title>Ann Linnson</Card.Title>
-                                <Card.Text>
-                                    Ann Linnson är forskare inom Vulkanologi och brinner för att hitta lösningar för att bevara jorden. 
-                                </Card.Text>
-                                <Button variant="info" href="mailto:#" >Kontakta Ann</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-            
-
-        );
-    }
-}
-    
-export default AboutUs;
