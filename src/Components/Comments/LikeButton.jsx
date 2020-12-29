eacb90968e050da35ce4527c79c10a7691035b2e
Lina <46371492+linahoggren@users.noreply.github.com>
2020-12-29 17:39:50 +0000

    Update LikeButton.jsx

--- src/Components/Comments/LikeButton.jsx
+++ src/Components/Comments/LikeButton.jsx
@@ -1,47 +1,151 @@
 import React, { Component } from "react";
+import { Button, Container } from 'react-bootstrap';
 import { faHeart as farHeart } from "@fortawesome/free-regular-svg-icons";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
-// const HeartIcon = <FontAwesomeIcon icon={faCoffee} />
 
 class LikeButton extends React.Component {
-    
-    state = {
+  
+  state = {
     likes: 0
+    
   };
-  
 
-  
+ 
   addLike = () => {
     let newCount = this.state.likes + 1;
       this.setState({
-      likes: newCount
+      likes: newCount,
+      bgColor: 'red'
     });
-    
-  };
-
-
-
   
+  
+}
+
+
+
+
+
   render() {
-	
+
       return (
-      <div>
-        <button 
-          className="likeButton" 
-          onClick={this.addLike} > <FontAwesomeIcon icon={farHeart} />
-          {this.state.likes} 
-        </button>
-      </div>
+      // <div>
+      //   <Button className="likeButton" 
+      //       > <FontAwesomeIcon icon={faHeart} onClick={this.addLike}  />
+      //     {this.state.likes} 
+      //   </Button>
+      // </div>
+
+
+        <Container className="likeBtnContainer">
+          <FontAwesomeIcon icon={faHeart} 
+          onClick={this.addLike} 
+          className="likeBtnIcon" style={{color:this.state.bgColor}} />
+          <span>
+           {this.state.likes} </span>
+          
+        </Container>
+
       );
   }
 }
     
 
+export default LikeButton;
 
 
+
+
+
+
+
+
+
+
+
+//  ---------------------- TEST 5 -------------------
+
+
+  // getInitialState = () => {
+  //   return {
+  //     bgColor: 'red'
+  //   }
+    
+  // }
+  
+  // handleClick = () => {
+  //   this.setState({
+  //     bgColor: 'red'
+  //   })
+  // }
+  
+  // render : function() {
+  //   return (
+  //     <div>
+  //       <button 
+  //         onClick={this.handleClick} 
+  //         style={{backgroundColor:this.state.bgColor}}>Button</button>
+  //     </div>
+  //   )
+  // }
+
+
+// ReactDOM.render(
+//   <ButtonColor />,
+//     document.getElementById('button')
+// );
+
+
+//  ny sektion test
+
+
+//  ---------------------- TEST 3 -------------------
+
+// class LikeButton extends React.Component {
+//   state = {
+//   likes: 0
+  
+// };
+
+
+
+// addLike = () => {
+//   let newCount = this.state.likes + 1;
+//     this.setState({
+//     likes: newCount
+//   });
+
+
+// }
+
+
+
+// render() {
+
+//   const activeLike = {
+//     color: "#17A2B8",
+    
+//   }
+
+
+//     return (
+    // <div>
+    //   <Button className="likeButton" 
+    //       > <FontAwesomeIcon icon={faHeart} onClick={this.addLike}  />
+    //     {this.state.likes} 
+    //   </Button>
+    // </div>
+//       <Container className="likeBtnContainer">
+//         <FontAwesomeIcon icon={faHeart} onClick={this.addLike} className="likeBtnIcon" /><span>
+//          {this.state.likes} </span>
+//       </Container>
+
+//     );
+// }
+// }
+  
 
 
 //  ---------------------- NYTT TEST from codepen -------------------
@@ -96,7 +200,7 @@ class LikeButton extends React.Component {
 // }
 
    
-        export default LikeButton;
+   
 
 
 
