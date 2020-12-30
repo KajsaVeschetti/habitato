1597e621b1d1459bf5cf1431c4cc8dd57930f5aa
Annica113 <45743272+Annica113@users.noreply.github.com>
2020-12-30 13:48:34 +0100

    Update popupTemp.jsx

--- src/Components/Charts/popup/popupTemp.jsx
+++ src/Components/Charts/popup/popupTemp.jsx
@@ -7,7 +7,8 @@ import { useSpring, animated } from 'react-spring/web.cjs'; // web.cjs is requir
 
 
 
-import InfoTemp from './infoTemp';
+//import InfoTemp from './infoTemp';
+import CarouSel from '../../CarouSel';
 
 //Nedan kod är hämtad från https://react.semantic-ui.com/modules/modal/ för att få till popup knappar på förstasidan.
 
@@ -90,7 +91,8 @@ export default function PopupTemp() {
         <Fade in={open}>
           <div className={classes.paper} style={{background:"rgba(68, 118, 128, 0.9)", overflow:"scroll", maxHeight:"600px", minHeight:"100%"}}>
             <h2 id="spring-modal-title" className="chartTitle">Global Temperatur</h2>
-            <InfoTemp/>
+            {/*<InfoTemp/>*/}
+            <CarouSel/>
           </div>
         </Fade>
       </Modal>
