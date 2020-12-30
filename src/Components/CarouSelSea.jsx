8dd1eb8d1c16f1310770f327a8eaab47a830d113
Annica113 <45743272+Annica113@users.noreply.github.com>
2020-12-30 16:53:19 +0100

    Create CarouSelSea.jsx

---
+++ src/Components/CarouSelSea.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import {Carousel} from 'react-bootstrap';
+import InfoSea from './Charts/popup/infoSea';
+import InfoTemp from './Charts/popup/infoTemp';
+import InfoCo2 from './Charts/popup/infoCO2';
+import InfoGlacier from './Charts/popup/infoGlacier';
+//import PopupCO2 from './Charts/popup/popupCO2';
+//import PopupTemp from './Charts/popup/popupTemp';
+//import PopupGL from './Charts/popup/popupGlacier';
+//import PopupSea from './Charts/popup/popupSea';
+
+
+class CarouSelSea extends Component {
+
+  render() {
+    return (
+
+<Carousel interval={null} style={{width:"750px"}} >
+<Carousel.Item active>
+    <div><InfoSea/></div>
+</Carousel.Item>
+<Carousel.Item>   
+    <div><InfoTemp/></div>
+</Carousel.Item>
+<Carousel.Item>
+    <div><InfoCo2/></div>
+</Carousel.Item>
+<Carousel.Item>
+   <div><InfoGlacier/></div>
+</Carousel.Item>  
+</Carousel>
+);
+}
+}
+
+
+  
+
+  export default CarouSelSea; 
