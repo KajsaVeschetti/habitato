ff2d47d1ba345f1fb61010e07e9b3d6314bbe643
Annica113 <45743272+Annica113@users.noreply.github.com>
2020-12-31 07:44:28 +0100

    Update popupTemp.jsx

--- src/Components/Charts/popup/popupTemp.jsx
+++ src/Components/Charts/popup/popupTemp.jsx
@@ -89,8 +89,7 @@ export default function PopupTemp() {
         }}
       >
         <Fade in={open}>
-          <div className={classes.paper} style={{background:"rgba(68, 118, 128, 0.9)", overflow:"scroll", maxHeight:"600px", minHeight:"100%"}}>
-            <h2 id="spring-modal-title" className="chartTitle">Global Temperatur</h2>
+        <div style={{ overflow:"scroll", maxHeight:"600px", minHeight:"100%", }}> 
             {/*<InfoTemp/>*/}
             <CarouSelTemp/>
           </div>
