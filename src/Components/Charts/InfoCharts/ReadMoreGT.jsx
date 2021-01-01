daef914428499f7b34598740862f9cd9e57e2fa1
Annica113 <45743272+Annica113@users.noreply.github.com>
2021-01-01 09:30:23 +0100

    Update ReadMoreGT.jsx

--- src/Components/Charts/InfoCharts/ReadMoreGT.jsx
+++ src/Components/Charts/InfoCharts/ReadMoreGT.jsx
@@ -6,7 +6,7 @@ function ReadMore() {
 
 
   const extraContent=
-    <div class="extra">
+    <div className="extra">
       <p>Global uppvärmning, eller växthuseffekten som det också kallas påverkar jordens temperatur. Det är när jordens medeltemperatur ökar som man brukar tala om global temperatur. Utan den naturliga växthuseffekten skulle inte vi människor kunna leva på jorden, problemet är den ökade växthuseffekten, den så kallade global uppvärmningen som vi människor bidrar till. De tre största orsakerna till utsläpp av växthusgaser är: utsläpp från fossila bränslen, från skogsavverkning och från jordbruket.
       </p>
   </div>
