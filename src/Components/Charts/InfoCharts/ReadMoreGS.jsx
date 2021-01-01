9b640af9b3d2f66622148f349977062b0f769eda
Annica113 <45743272+Annica113@users.noreply.github.com>
2021-01-01 09:30:09 +0100

    Update ReadMoreGS.jsx

--- src/Components/Charts/InfoCharts/ReadMoreGS.jsx
+++ src/Components/Charts/InfoCharts/ReadMoreGS.jsx
@@ -7,7 +7,7 @@ function ReadMore() {
 
 
   const extraContent=
-    <div class="extra">
+    <div className="extra">
       <p>
       Glaciär är stora isblock. Glaciärer finns på olika ställen på jorden. Den arktiska isen som täcker havet runt Nordpolen är en glaciär som minskar i storlek allt snabbare. Den globala uppvärmningen bidrar till att mer is smälter bort under sommaren och att mindre ny is bildas på vintern. 
       Det är en stor risk att all is i Arktis kommer att smälta bort innan år 2100. Forskarna är oroliga för hur resten av jorden kommer att påverkas när all is är borta, eftersom det ofta sägs att Arktis fungerar som jordens kylskåp. Vit is reflekterar solljuset, till skillnad från ett mörkt hav som tar upp solenergi och blir varmare
