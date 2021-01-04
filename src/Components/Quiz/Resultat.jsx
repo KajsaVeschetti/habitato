45f94dfcd46f8808e004578640b84158c3fbf08d
Annica113 <45743272+Annica113@users.noreply.github.com>
2021-01-04 13:41:57 +0100

    Update Resultat.jsx

--- src/Components/Quiz/Resultat.jsx
+++ src/Components/Quiz/Resultat.jsx
@@ -7,12 +7,12 @@ import { Button } from 'react-bootstrap';
 
 
 const Resultat = ({score, playAgain, responses }) => 
-<div class="contentWrap" >
+<div className="contentWrap" >
     <div> 
         <h2>Du fick {score} av {responses} möjliga </h2> 
     </div>
 
-    <div class="btnWrap">
+    <div className="btnWrap">
         <Button variant="info" size="sm"onClick={playAgain}>Gör om Quiz</Button>
     </div>
     <Facit />
