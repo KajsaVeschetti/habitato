d602a04923b764edb41cd86c296c7fa29ed17bba
Annica113 <45743272+Annica113@users.noreply.github.com>
2021-01-04 13:44:07 +0100

    Update infoGlacier.jsx

--- src/Components/Charts/popup/infoGlacier.jsx
+++ src/Components/Charts/popup/infoGlacier.jsx
@@ -65,11 +65,11 @@ class InfoGlacier extends Component {
             <div style={{background:"rgba(0, 144, 170, 0.9)", paddingBottom: "15px", border:'2px solid #000'}}>
             <h2 id="spring-modal-title"className="chartTitle pt-3">Glaciärstorlek</h2>
 
-              <div class="card mx-auto charts " style={{width:"750px"}}>
+              <div className="card mx-auto charts " style={{width:"750px"}}>
               <div className="chartHeader">
               <h3>Skriv in två årtal och se vad <br/> klimatförändringarna har gjort under åren</h3>
               </div>
-              <div class="mx-auto chartContainer"> 
+              <div className="mx-auto chartContainer"> 
                         <div component={Glacier}>
                         <Glacier
                         chartName="Glacier1"
