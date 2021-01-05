2ea0c33b3eb7766108a2cf73dc72c91e3579adc3
Annica113 <45743272+Annica113@users.noreply.github.com>
2021-01-05 02:26:03 +0100

    Update FaktaRutor.jsx

--- src/Components/Fakta/FaktaRutor.jsx
+++ src/Components/Fakta/FaktaRutor.jsx
@@ -1,48 +1,55 @@
-import React, { useState } from 'react';
-import FossilFuel from './FossilFuel'; 
-import GlobalWarming from './GlobalWarming'; 
-import GlacierSize from './GlacierSize'; 
-import SeaLevel from './SeaLevel'; 
-import {Container, Card, Tabs, Tab} from 'react-bootstrap';
+import React, { useState } from 'react'; //Importerar useState hook från React.
+import {Container, Card, Tabs, Tab} from 'react-bootstrap'; //Importerar Container, Card, Tabs och Tab från react-bootstrap
+import FossilFuel from './FossilFuel'; //Importerar FossilFuel från mappen Faktarutor
+import GlobalWarming from './GlobalWarming'; //Importerar GlobalWarming från mappen Faktarutor
+import GlacierSize from './GlacierSize'; //Importerar GlacierSiza från mappen Faktarutor
+import SeaLevel from './SeaLevel'; //Importerar SeaLevel från mappen Faktarutor
 
 
 function Fakta (){
+  //En ny state-variabel deklareras genom att kalla på useState hook. Den returnerar ett par värden som namngetts.
+  //Variabeln kallas keyTab eftersom den håller värdena för flikarna. Flikens värde sätts till "Fossilt bränsle"
+  //eftersom det visar vad som är själva utgångsläget. Den andra delen, som själv är en funktion, gör att flikarnas 
+  //nuvarande värde uppdateras.
+
    const [keyTab, setKeyTab] = useState();
    return(
    <>
-    <Container className="charts col-lg-9 col-xs-12 px-lg-5 pb-5">
-      <h1>Fakta om klimatförändringar</h1> 
+     <Container className="charts col-lg-9 col-xs-12 px-lg-5 pb-5">
+       <h1>Fakta om klimatförändringar</h1>
       <Card className="bg-light pb-3 px-lg-5">   
-     <Card.Body>
+      <Card.Body>
 
-      <Tabs 
-       id="Fakta-tab"
+     {/*När man trycker på en namngiven flik ändras innehållet så det överenstämmer med titeln (nav med flikar). Flikarna är responsiva*/}
+     
+     
+     <Tabs      
        className="d-lg-flex d-block"
-       onSelect={(key) => setKeyTab(key)}
        activeKey={keyTab}
+       onSelect={(k) => setKeyTab(k)} 
       >
-      <Tab eventKey ="Fossilt bränsle" title="Fossilt bränsle">
+      <Tab eventKey ="Fossilt bränsle" title="Fossilt bränsle" tabClassName="profile-tabitem1" >
        <div className="mt-5">
         <FossilFuel/>
        </div>
       </Tab>
-      <Tab eventKey="Global uppvärmning" title="Global uppvärmning">
+      <Tab eventKey="Global uppvärmning" title="Global uppvärmning" tabClassName="profile-tabitem2">
        <div className="mt-5">
         <GlobalWarming/>
        </div> 
       </Tab>
-      <Tab eventKey="Glaciärstorlek" title="Glaciärstorlek">
+      <Tab eventKey="Glaciärstorlek" title="Glaciärstorlek" tabClassName="profile-tabitem3">
        <div className="mt-5">
         <GlacierSize/>
        </div>
       </Tab>
-      <Tab eventKey="Havsnivåer" title="Havsnivåer">
+      <Tab eventKey="Havsnivå" title="Havsnivå" tabClassName="profile-tabitem4">
        <div className="mt-5">
         <SeaLevel/>
        </div>  
       </Tab>
       </Tabs>
-
+  
      </Card.Body>
      </Card>
     </Container>
@@ -53,3 +60,6 @@ function Fakta (){
 
        
 export default Fakta;
+
+
+
