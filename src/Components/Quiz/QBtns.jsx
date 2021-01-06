de9c86030b078d1dda57c745885278a23f0486a6
Jenniehwest <50371951+Jenniehwest@users.noreply.github.com>
2021-01-06 08:16:32 +0100

    Update QBtns.jsx

--- src/Components/Quiz/QBtns.jsx
+++ src/Components/Quiz/QBtns.jsx
@@ -1,25 +1,58 @@
+// import React, { useState } from "react";
+// import { Button} from 'react-bootstrap';
+
+
+
+// const QBtns = ({ question, options, selected }) => {
+//     // state kan bara användas i class. UseState returns en array inte objekt
+//     const [answer, setAnswer] = useState(options); //här gör det så att man kan använda statevariabeln i en funktionen.
+//     return (
+//         <div>
+//             <div>{question}</div>
+//                 <div className="questWrap">
+//                     {/* mappar ut knappar */}
+//                 {answer.map((text) => (
+//                     <Button className="btnWrap" variant="info" size="sm" onClick={() => {
+//                         setAnswer([text]);
+//                         selected(text);
+//                     }}>
+//                         {text}
+//                     </Button>
+//                 ))}
+//                 </div>
+//         </div>
+//     )
+// }
+// export default QBtns;
+
+
 import React, { useState } from "react";
-import { Button} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 
 
 const QBtns = ({ question, options, selected }) => {
     // state kan bara användas i class. UseState returns en array inte objekt
     const [answer, setAnswer] = useState(options); //här gör det så att man kan använda statevariabeln i en funktionen.
+    const [alreadyAnswered, setAlreadyAnswered] = useState(0);
+
     return (
         <div>
             <div>{question}</div>
-                <div className="questWrap">
-                    {/* mappar ut knappar */}
+            <div className="questWrap">
+                {/* mappar ut knappar */}
                 {answer.map((text) => (
                     <Button className="btnWrap" variant="info" size="sm" onClick={() => {
-                        setAnswer([text]);
-                        selected(text);
+                        if (!alreadyAnswered) {
+                            setAnswer([text]);
+                            selected(text);
+                            setAlreadyAnswered(1);
+                        }
                     }}>
                         {text}
                     </Button>
                 ))}
-                </div>
+            </div>
         </div>
     )
 }
