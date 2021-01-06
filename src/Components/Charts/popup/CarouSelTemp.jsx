39588bbca86936a87337027277c764f8ade00943
Kajsa Veschetti <46371230+KajsaVeschetti@users.noreply.github.com>
2021-01-06 14:03:55 +0100

    Create CarouSelTemp.jsx

---
+++ src/Components/Charts/popup/CarouSelTemp.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {Carousel} from 'react-bootstrap';
+import InfoTemp from './infoTemp';
+import InfoCo2 from './infoCO2';
+import InfoGlacier from './infoGlacier';
+import InfoSea from './infoSea';
+
+
+
+class CarouSelTemp extends Component {
+
+  render() {
+    return (
+
+<Carousel interval={null} indicators={false} style={{width:"785px", height: "800px"}}>
+<Carousel.Item>  
+    <div><InfoTemp/></div>
+</Carousel.Item>
+<Carousel.Item>   
+    <div><InfoCo2/></div>
+</Carousel.Item>
+<Carousel.Item> 
+    <div><InfoSea/></div>
+    </Carousel.Item>  
+    <Carousel.Item> 
+   <div><InfoGlacier/></div>
+</Carousel.Item>  
+</Carousel>
+);
+}
+}
+
+
+  
+
+  export default CarouSelTemp; 
