ec1e0bfdc7638544c695e6d878d1f94dc4292028
Kajsa Veschetti <46371230+KajsaVeschetti@users.noreply.github.com>
2021-01-06 14:03:44 +0100

    Create CarouSelCo2.jsx

---
+++ src/Components/Charts/popup/CarouSelCo2.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {Carousel} from 'react-bootstrap';
+import InfoCo2 from './infoCO2';
+import InfoGlacier from './infoGlacier';
+import InfoSea from './infoSea';
+import InfoTemp from './infoTemp';
+
+
+
+class CarouSelCo2 extends Component {
+
+  render() {
+    return (
+
+<Carousel interval={null} indicators={false} style={{width:"785px", height:"800px"}} >
+<Carousel.Item>  
+    <div><InfoCo2/></div>
+</Carousel.Item>
+<Carousel.Item>     
+    <div><InfoSea/></div>
+</Carousel.Item>
+<Carousel.Item>  
+    <div><InfoGlacier/></div>
+</Carousel.Item>
+<Carousel.Item>  
+   <div><InfoTemp/></div>
+</Carousel.Item>  
+</Carousel>
+);
+}
+}
+
+
+  
+
+  export default CarouSelCo2; 
