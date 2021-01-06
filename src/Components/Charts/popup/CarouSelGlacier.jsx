5d64c17251dacf5fade0271b5d4b6c3564034ffb
Kajsa Veschetti <46371230+KajsaVeschetti@users.noreply.github.com>
2021-01-06 14:03:48 +0100

    Create CarouSelGlacier.jsx

---
+++ src/Components/Charts/popup/CarouSelGlacier.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {Carousel} from 'react-bootstrap';
+import InfoGlacier from './infoGlacier';
+import InfoSea from './infoSea';
+import InfoTemp from './infoTemp';
+import InfoCo2 from './infoCO2';
+
+
+
+class CarouSelGlacier extends Component {
+
+  render() {
+    return (
+
+<Carousel interval={null} indicators={false} style={{width:"785px", height:"800px"}} >
+<Carousel.Item>  
+    <div><InfoGlacier/></div>
+</Carousel.Item>
+<Carousel.Item>     
+    <div><InfoTemp/></div>
+</Carousel.Item>
+<Carousel.Item>  
+    <div><InfoCo2/></div>
+</Carousel.Item>
+<Carousel.Item>  
+   <div><InfoSea/></div>
+</Carousel.Item>  
+</Carousel>
+);
+}
+}
+
+
+  
+
+  export default CarouSelGlacier; 
