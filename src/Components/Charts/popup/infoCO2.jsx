379408c6d4bb8943833e52bd67e69764fda28e20
Annica113 <45743272+Annica113@users.noreply.github.com>
2021-01-07 17:30:07 +0100

    Update infoCO2.jsx

--- src/Components/Charts/popup/infoCO2.jsx
+++ src/Components/Charts/popup/infoCO2.jsx
@@ -69,7 +69,6 @@ class InfoCo2 extends Component {
               <h4>Skriv in ett årtal mellan 1751-2014 och tryck på enter för att få fram värden.</h4>
             </div>
                         
-<<<<<<< Updated upstream
                 <div className="chartContainer mx-auto"> 
                  <div component={BarCharts}>
                     <BarCharts 
@@ -84,23 +83,6 @@ class InfoCo2 extends Component {
                    <ReadMore/> 
                  </div>   
                 <div>
-=======
-                        <div className="chartContainer mx-auto"> 
-                        <div component={BarCharts}>
-                        <BarCharts 
-                        chartName="Bar1"
-                        CO2Emission={this.state.CO2Emission[this.state.indexYear1]} 
-                        onYearFilter={this.handleYearFilter}/>
-                        <BarCharts
-                        chartName="Bar2"
-                        CO2Emission={this.state.CO2Emission[this.state.indexYear2]} 
-                        onYearFilter={this.handleYearFilter}/>
-                       {/* Co2-info */}
-                       <ReadMore/> 
-                       <ReadMoreButton/>
-                        </div>   
-                                      <div>
->>>>>>> Stashed changes
                                                     
                </div>  
                </div>       
