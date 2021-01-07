cbfab5fa9d483dd795de1a65b67b8ba55b39263a
Lina <46371492+linahoggren@users.noreply.github.com>
2021-01-07 11:15:45 +0000

    Update Page.jsx

--- src/Components/Page.jsx
+++ src/Components/Page.jsx
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 // import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import ReadMoreButton from "./btn"
 
 // import Test from "./Quiz/Test";
 import Kontakt from "./ContactAboutUs/Kontakt";
@@ -18,7 +19,7 @@ class Page extends Component {
      <div className="wrapper">
        <div className="contentWrap">
         <NavBar/>
-   
+      <ReadMoreButton/>
    
       </div>
      
