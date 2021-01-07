688497d2e23f1ba50ae30921d4c75fc5d0026d90
Lina <46371492+linahoggren@users.noreply.github.com>
2021-01-07 11:15:33 +0000

    Create readMore.js

---
+++ src/Components/data/readMore.js
@@ -0,0 +1,42 @@
+const textArray = [
+    {
+        "id": "co2",
+        "text": "Co2 är den kemiska formeln för Koldioxid. Koldioxid är en färg- och luktfri gas. Runt jorden, i vår atmosfär, finns det flera olika gaser, bland annat koldioxid. Men även metan, lustgas och andra så kallade växthusgaser. Växthusgaserna hjälper till att hålla jorden varm, utan den naturliga växthuseffekten skulle vår jord vara ungefär 30 grader kallare än vad den är idag. När växthusgaserna ökar i atmosfären förstärks den naturliga växthuseffekten, vilket leder till att jordens medeltemperatur ökar. Det är den här temperaturökningen som kallas för global uppvärmning."
+    },
+    {
+        "id": "gt",
+        "text": "hguejdisoaåghuedwisoaåghewauioå"
+    },
+    {
+        "id": "gs",
+        "text": "hguejdisoaåghuedwisoaåghewauioå"
+    },
+    {
+        "id": "sl",
+        "text": "hguejdisoaåghuedwisoaåghewauioå"
+    },
+    {
+        "id": "karl",
+        "text": "hguejdisoaåghuedwisoaåghewauioå"
+    },
+    {
+        "id": "lars",
+        "text": "hguejdisoaåghuedwisoaåghewauioå"
+    },
+    {
+        "id": "linn",
+        "text": "hguejdisoaåghuedwisoaåghewauioå"
+    },
+    {
+        "id": "ann",
+        "text": "Ann Linnson är forskare inom Vulkanologi och brinner för att hitta lösningar för att bevara jorden. "
+    }
+    
+    
+];
+
+
+export function getReadMore() {
+	return textArray;
+}
+
