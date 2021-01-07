8ab293816ac86f071baaf3023c17b6ca0d5a116f
Kajsa Veschetti <46371230+KajsaVeschetti@users.noreply.github.com>
2021-01-07 02:43:37 +0100

    Update SeaLevel.jsx

--- src/Components/Charts/diagram/SeaLevel.jsx
+++ src/Components/Charts/diagram/SeaLevel.jsx
@@ -4,6 +4,9 @@ import React, { Component } from 'react';
     BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
   } from 'recharts';
   import PropTypes from 'prop-types';
+
+
+  
   const getPath = (x, y, width, height) => `M${x},${y + height}
   C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3} ${x + width / 2}, ${y}
   C${x + width / 2},${y + height / 3} ${x + 2 * width / 3},${y + height} ${x + width}, ${y + height}
@@ -44,7 +47,28 @@ class Sea extends Component {
     ];
 
     const COLORS =[ "#ff595e", "#f8961e", "#8ac926", "#1982c4", "#6a4c93" ] 
-        return ( 
+    
+    function getIntroOfPage(label) {
+      if (label === 'GMSL') {
+        return 'GMSL betyder global mean sea level. På svenska global genomsnittlig havsnivå. Detta är rekonsturerad genbomsnittlig havsnivå i mm ';
+      } if (label === 'GMSL ovisshet') {
+        return 'Denna spalt visar den ovisshet som orsakas av rekonstruktionen.';
+      } 
+    }
+    
+    function CustomTooltip({ payload, label, active }) {
+      if (active) {
+        return (
+          <div className="custom-tooltip" style={{width:"200px", border:"1px solid white", background:"rgba(249, 249, 249, 0.8)"}}>
+            <p className="label">{`${label} : ${payload[0].value}`}</p>
+            <p className="intro">{getIntroOfPage(label)}</p>
+          </div>
+        );
+      }
+      return null;
+    }
+    
+    return ( 
             <React.Fragment>
               <div style={{width:"300px", height:"400px", float:"left"}}>
               <div style={{textAlign:"center", marginLeft:"3.5em"}}>
@@ -59,10 +83,11 @@ class Sea extends Component {
                     }}>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="name" />
-                <YAxis />
+                <YAxis type= "number" domain={[-190, 190]}/>
+                <Tooltip content={<CustomTooltip/>}/>
                 <Bar dataKey="SEA" fill="#8884d8" shape={<TriangleBar/>} label={{ position: 'top' }}> 
                 <Cell  fill ={COLORS[3]}/>
-                <Cell  fill ={COLORS[24]}/>
+                <Cell  fill ={COLORS[4]}/>
                 </Bar>
        
                 </BarChart> 
