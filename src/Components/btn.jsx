b299902a6d080822836a51b9b141517b2bcb685d
Kajsa Veschetti <46371230+KajsaVeschetti@users.noreply.github.com>
2021-01-09 00:02:13 +0100

    Update btn.jsx

--- src/Components/btn.jsx
+++ src/Components/btn.jsx
@@ -1,22 +1,43 @@
 import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
+import { Item } from 'semantic-ui-react';
 
 
-function ReadMoreButton () {
-    
-        const [readMore,setReadMore]=useState(false);
+import { infoData } from './data/readMore';
 
-            const BtnName=
-            readMore?'Göm info ':'Läs mer'
+ function ReadMoreButton ({id, text}) {
+         const [readMore,setReadMore]=useState(false);
+ 
+
+         const BtnName=
+         readMore?'Göm info ':'Läs mer'
+
+        
+         const extraContent= 
+          
+         // Variabel att förvara text man vill visa/gömma. 
+            <div className="extra">
+              {infoData.map((data,index) => {
+                for (let i=0; i < 7; i++){
+                return(
+                 <p key={index}> 
+                     {data.name}
+                </p>
+                );
+               }
+             
+              })}
+             
+          </div>
             
-            return (
-              <div className="App">
-                <Button size="sm" variant="info" onClick={()=>{setReadMore(!readMore)}}>{BtnName}</Button> 
-              </div>
+             return (
+               <div className="App">
+                 <Button size="sm" variant="info" onClick={()=>{setReadMore(!readMore)}}>{BtnName}</Button> {readMore && extraContent}
+               </div>
             
-            );
+             );
        
+ }
     
-}
-    
-export default ReadMoreButton;
+ export default ReadMoreButton;
+
