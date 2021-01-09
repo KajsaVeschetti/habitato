086da8f1a5b3ea0755a82eb4958278c56a25fd35
Lina <46371492+linahoggren@users.noreply.github.com>
2021-01-09 22:47:41 +0000

    Update LikeButton.jsx

--- src/Components/Comments/LikeButton.jsx
+++ src/Components/Comments/LikeButton.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import {  Container } from 'react-bootstrap';
-// import { faHeart as farHeart } from "@fortawesome/free-regular-svg-icons";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 class LikeButton extends React.Component {
-  state = {
-  likes: 0
+  state = {                                  // listar komponentens data som hanteras
+  likes: 0                                   // state-objektets attribut
   };
 
-  addLike = () => {
+  addLike = () => {                         // arrow function som räknar
     let newCount = this.state.likes + 1;
     this.setState({
       likes: newCount,
@@ -25,6 +24,7 @@ class LikeButton extends React.Component {
           icon={faHeart} 
           onClick={this.addLike} 
           className="likeBtnIcon" 
+          title="Gillaknapp"
           style={{color:this.state.bgColor}}/>
         <span>
            {this.state.likes} 
@@ -39,154 +39,12 @@ export default LikeButton;
 
 
 
+// Referenser:
 
+// https://medium.com/javascript-in-plain-english/creating-a-reusable-like-button-component-in-react-8028d17c024c
 
+// https://stackoverflow.com/questions/59091882/how-can-i-use-outlined-icons-with-react-fontawesome
 
+// https://stackoverflow.com/questions/56935208/change-the-color-when-onclick-with-react
 
-
-
-
-//  ---------------------- TEST 5 -------------------
-
-
-  // getInitialState = () => {
-  //   return {
-  //     bgColor: 'red'
-  //   }
-    
-  // }
-  
-  // handleClick = () => {
-  //   this.setState({
-  //     bgColor: 'red'
-  //   })
-  // }
-  
-  // render : function() {
-  //   return (
-  //     <div>
-  //       <button 
-  //         onClick={this.handleClick} 
-  //         style={{backgroundColor:this.state.bgColor}}>Button</button>
-  //     </div>
-  //   )
-  // }
-
-
-// ReactDOM.render(
-//   <ButtonColor />,
-//     document.getElementById('button')
-// );
-
-
-//  ny sektion test
-
-
-//  ---------------------- TEST 3 -------------------
-
-// class LikeButton extends React.Component {
-//   state = {
-//   likes: 0
-  
-// };
-
-
-
-// addLike = () => {
-//   let newCount = this.state.likes + 1;
-//     this.setState({
-//     likes: newCount
-//   });
-
-
-// }
-
-
-
-// render() {
-
-//   const activeLike = {
-//     color: "#17A2B8",
-    
-//   }
-
-
-//     return (
-    // <div>
-    //   <Button className="likeButton" 
-    //       > <FontAwesomeIcon icon={faHeart} onClick={this.addLike}  />
-    //     {this.state.likes} 
-    //   </Button>
-    // </div>
-//       <Container className="likeBtnContainer">
-//         <FontAwesomeIcon icon={faHeart} onClick={this.addLike} className="likeBtnIcon" /><span>
-//          {this.state.likes} </span>
-//       </Container>
-
-//     );
-// }
-// }
-  
-
-
-//  ---------------------- NYTT TEST from codepen -------------------
-
-
-// class LikeButton extends React.Component {
-//   state = {
-//     likes: 0
-//   };
-
-//   addLike = () => {
-//     let newCount = this.state.likes + 1;
-//      this.setState({
-//      likes: newCount
-//      });
-//   };
-
-//   render() {
-//     const likes = this.state.likes;
-//     if (likes === 0) {
-//       return (
-//         <div>
-//           <button
-//             className="button"
-//             onClick={this.addLike}
-//           >
-//             <i className="far fa-heart fa-lg" style={{ color: "#33c3f0" }}></i>
-//           </button>
-//         </div>
-//       );
-//     }
-//     if (likes === 1) {
-//       return (
-//         <div>
-//           <button className="button" onClick={this.addLike}>
-//             <i className="fas fa-heart fa-lg" style={{ color: "red" }}></i>
-//           </button>
-//         </div>
-//       );
-//     }
-//     if (likes > 1) {
-//       return (
-//         <div>
-//           <button className="button" onClick={this.addLike}>
-//             <i className="fas fa-heart fa-lg" style={{ color: "red" }}></i>{" "}
-//             {likes}
-//           </button>
-//         </div>
-//       );
-//     }
-//   }
-// }
-
-   
-   
-
-
-
-        // https://medium.com/javascript-in-plain-english/creating-a-reusable-like-button-component-in-react-8028d17c024c
-        // https://stackoverflow.com/questions/59091882/how-can-i-use-outlined-icons-with-react-fontawesome
-        // https://stackoverflow.com/questions/56935208/change-the-color-when-onclick-with-react
-        // https://forum.freecodecamp.org/t/react-js-i-need-a-button-color-to-change-onclick-but-cannot-determine-how-to-properly-set-and-change-state-for-that-component/45168/2
-        // 
+// https://forum.freecodecamp.org/t/react-js-i-need-a-button-color-to-change-onclick-but-cannot-determine-how-to-properly-set-and-change-state-for-that-component/45168/2
