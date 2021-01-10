ac6c49b641a880459066da2d6e6332d6f0388754
Kajsa Veschetti <46371230+KajsaVeschetti@users.noreply.github.com>
2021-01-10 03:25:08 +0100

    Update CarouSelSea.jsx

--- src/Components/Charts/popup/CarouSelSea.jsx
+++ src/Components/Charts/popup/CarouSelSea.jsx
@@ -12,7 +12,7 @@ class CarouSelSea extends Component {
   render() {
     return (
 
-<Carousel interval={null} indicators={false} style={{width:"785px", height:"600px"}} >
+<Carousel interval={null} indicators={false} style={{width:"1000px", height:"660px"}} >
 <Carousel.Item>  
     <div><InfoSea/></div>
 </Carousel.Item>  
