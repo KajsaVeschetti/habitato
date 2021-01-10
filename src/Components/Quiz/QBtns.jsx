9cef3930db1c036d37929a33b7d9a6bc7a40bdd4
Jenniehwest <50371951+Jenniehwest@users.noreply.github.com>
2021-01-10 11:26:28 +0100

    Update QBtns.jsx

--- src/Components/Quiz/QBtns.jsx
+++ src/Components/Quiz/QBtns.jsx
@@ -2,23 +2,27 @@ import React, { useState } from "react";
 import { Button } from 'react-bootstrap';
 
 
+//Funktion för att få knappar till quiz
+// Tar in objekt och lägger i state
+//För att kunna använda statevariabeln i en funktion
+
+//En boolean i state, påminner om readmore, "toggla" inte men ändrat till true när användaren klickat på ett svar.
 
 const QBtns = ({ question, options, selected }) => {
-    // state kan bara användas i class. UseState returns en array inte objekt
-    const [answer, setAnswer] = useState(options); //här gör det så att man kan använda statevariabeln i en funktionen.
-    const [alreadyAnswered, setAlreadyAnswered] = useState(0);
+    const [answer, setAnswer] = useState(options); 
+    const [alreadyAnswered, setAlreadyAnswered] = useState(false);
 
     return (
         <div>
-            <div className="abc">{question}</div>
+            <div className="wrap">{question}</div>
             <div className="questWrap">
-                {/* mappar ut knappar */}
+                {/* mappar ut text på knappar */}
                 {answer.map((text) => (
                     <Button className="btnWrap" variant="info" size="block" onClick={() => {
                         if (!alreadyAnswered) {
                             setAnswer([text]);
                             selected(text);
-                            setAlreadyAnswered(1);
+                            setAlreadyAnswered(true);
                         }
                     }}>
                         {text}
