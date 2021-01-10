fcea95b10bb6e9ca2a7fecc64b3e671b1d20ad18
Jenniehwest <50371951+Jenniehwest@users.noreply.github.com>
2021-01-10 10:42:44 +0100

    Update Quiz.jsx

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -14,7 +14,7 @@ class Quiz extends Component {
     }
 
 // Tar in objekt och lägger i state
-// funktionen ser om svar matchar med rätt svar och lägger till poång i statevariabel score
+// Ser om svar matchar med rätt svar och lägger till poång i statevariabel score
 computeAnswer = (answer, correct_answer) => {
     if (answer  === correct_answer){
         this.setState({
@@ -22,48 +22,46 @@ computeAnswer = (answer, correct_answer) => {
         });
     }
     
-    //*conditional (ternary) operator* Kort version av if else
-   // ett villkor följt av ett frågtecken
-   // Sen vad som ska utföras om villkoret är sant följt av kolon
-   //till sist vad som ska utgföras om villkoret är falskt. 
-   //*condition ? exprIfTrue : exprIfFalse*
-   
+   //Räknar antal svar till statevariabel responses
     this.setState({
         responses: this.state.responses < 10 ? this.state.responses + 1 : 10 
     });
 };
 
-// Gör quiz igen. funktion nollställer poäng och svar
+// Om användaren väljer att göra quizet igen. Funktionen nollställer poäng och svar
 playAgain = () => {
     this.setState({
         score: 0,
         responses: 0
     })
 }
+//Funktion som hämtar frågor från js-filen och förvarar i state questionArray
      async componentDidMount () {
         this.setState({
             questionArray: getQuestions(),
         });
     }
+
+    /* renderar quiz med hjälp av data från state */
     render() { 
         return ( 
             <Container className="charts col-lg-9">
-
             <div className="container">
                 <h1>Habitatos miljöquiz</h1>
-
-                {/* renderar data från state */}
                 <div className="chartContainer">
                 {this.state.questionArray.length > 0 && 
                 this.state.responses < 10 &&
-                    this.state.questionArray.map(({question, correct_answer, answer, questionId}) => 
+
+                //mappar igenom information i js-fil och deklarerar vad som är vad
+                //till komponentenen Qbtns
+
+                //om antal svar är exakt 10 visas resultatkomponent.
+                //Deklarerar vad som är vad för Resultatkomponent
+                    this.state.questionArray.map(({question, correct_answer, answer}) => 
                     <QBtns question={question} 
-                    options={correct_answer, answer} key={questionId}
-                    selected={answer => this.computeAnswer(answer, correct_answer)}
-                />
+                    options={correct_answer, answer} 
+                    selected={answer => this.computeAnswer(answer, correct_answer)}/>
                 )}
-{/* om antal responses är exakt 10 visa komponent resultat. 
-Deklarerar vad state score, playAgian och responsens är. */}
                 {this.state.responses  === 10 ? (  <Resultat score={this.state.score} playAgain={this.playAgain} responses={this.state.responses} /> ) : null } 
                 </div>   
             </div>
