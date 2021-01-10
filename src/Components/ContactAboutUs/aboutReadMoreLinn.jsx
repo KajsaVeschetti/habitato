150110e6dddae178364325ae8c0b44899de74ab6
Lina <46371492+linahoggren@users.noreply.github.com>
2021-01-10 16:41:10 +0000

    Update aboutReadMoreLinn.jsx

--- src/Components/ContactAboutUs/aboutReadMoreLinn.jsx
+++ src/Components/ContactAboutUs/aboutReadMoreLinn.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 import { Card, Button } from 'react-bootstrap';
 
-function ReadMoreLinn() {
-    const [readMore,setReadMore]=useState(false);
+function ReadMoreLinn() {                            // skapar funktionen ReadMoreLinn
+    const [readMore,setReadMore]=useState(false);    // togglar mellan att visa och gömma innehållet.
   
   
-    const extraContent=
+    const extraContent=                               // Const som håller innehållet som ska visas eller gömmas
       <Card.Text className="conAboutPerson">
         Linn Annson är professor i Social-antropologi med intresse för samhället och kultur. 
       </Card.Text>
     
-    const BtnName=
+    const BtnName=                                    // Håller texterna som knappen togglar mellan
     readMore?'Göm info ':'Om Linn '
-    
+                                                      // Nedan renderas en knapp med ett onClick event som startar setReadMore funktionen
+                                                      // Readmore - Växlar mellan knappnamn beroende på vad den gör
+                                                      // btnname - namnen som knappen ska toggla mellan.
+                                                      //extraContent= min text som ska visat
     return (
       <div className="conReadMoreBtn">
         <Button 
@@ -25,4 +28,4 @@ function ReadMoreLinn() {
     );
   }
   
-  export default ReadMoreLinn;
+  export default ReadMoreLinn;                         // exporterar funktionen ReadMoreLinn
