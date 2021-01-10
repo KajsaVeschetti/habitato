b2d5904712e84868e8f5f7ad762dbe48efbb47c4
Lina <46371492+linahoggren@users.noreply.github.com>
2021-01-10 16:41:15 +0000

    Update aboutReadMoreLars.jsx

--- src/Components/ContactAboutUs/aboutReadMoreLars.jsx
+++ src/Components/ContactAboutUs/aboutReadMoreLars.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 import { Card, Button } from 'react-bootstrap';
 
-function ReadMoreLars() {
-    const [readMore,setReadMore]=useState(false);
+function ReadMoreLars() {                             // skapar funktionen ReadMoreLars
+    const [readMore,setReadMore]=useState(false);     // togglar mellan att visa och gömma innehållet.
     
-    const extraContent=
+    const extraContent=                                 // Const som håller innehållet som ska visas eller gömmas
       <Card.Text className="conAboutPerson">
         Lars Karlsson är professor i Biologi och Etologi och intresserar sig mycket i natur, blommor och bin.
       </Card.Text>
     
-    const BtnName=
+    const BtnName=                                       // Håller texterna som knappen togglar mellan
     readMore?'Göm info ':'Om Lars '
-    
-    return (
+                                                        // Nedan renderas en knapp med ett onClick event som startar setReadMore funktionen
+                                                        // Readmore - Växlar mellan knappnamn beroende på vad den gör
+                                                        // btnname - namnen som knappen ska toggla mellan.
+                                                        //extraContent= min text som ska visat 
+  return (
       <div className="conReadMoreBtn">
         <Button 
           block
@@ -24,4 +27,4 @@ function ReadMoreLars() {
     );
   }
   
-  export default ReadMoreLars;
+  export default ReadMoreLars;      // exporterar funktionen ReadMoreLars
