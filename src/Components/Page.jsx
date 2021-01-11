eed3dc2bcdd4b81d82a9d49f71cc4c649c137cd6
Kajsa Veschetti <46371230+KajsaVeschetti@users.noreply.github.com>
2021-01-11 09:09:03 +0100

    Update Page.jsx

--- src/Components/Page.jsx
+++ src/Components/Page.jsx
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 // import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-import ReadMoreButton from "./btn"
+
 
 // import Test from "./Quiz/Test";
 import Kontakt from "./ContactAboutUs/Kontakt";
