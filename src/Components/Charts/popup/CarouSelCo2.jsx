a99987e13e932ac0d0b2f0fae28ff6132a3b46f2
Annica113 <45743272+Annica113@users.noreply.github.com>
2021-01-11 08:59:32 +0100

    Update CarouSelCo2.jsx

--- src/Components/Charts/popup/CarouSelCo2.jsx
+++ src/Components/Charts/popup/CarouSelCo2.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Carousel} from 'react-bootstrap';
+import {Carousel} from 'react-bootstrap'; //importerar Carousel från react-bootstrap
 import InfoCo2 from './infoCO2';
 import InfoGlacier from './infoGlacier';
 import InfoSea from './infoSea';
@@ -10,27 +10,30 @@ import InfoTemp from './infoTemp';
 class CarouSelCo2 extends Component {
 
   render() {
+    {/*Hämtad från react bootstrap för att skapa en karuell med slider och pilar. Utan intervall och indikatorer */}  
     return (
-
-<Carousel interval={null} indicators={false} style={{width:"1000px", height:"660px"}} >
-<Carousel.Item>  
-    <div><InfoCo2/></div>
-</Carousel.Item>
-<Carousel.Item>     
-    <div><InfoSea/></div>
-</Carousel.Item>
-<Carousel.Item>  
+      <Carousel interval={null} indicators={false} style={{width:"1000px", height:"660px"}} > 
+        <Carousel.Item>  
+           <div><InfoCo2/></div>
+        </Carousel.Item>
+        <Carousel.Item>     
+           <div><InfoSea/></div>
+        </Carousel.Item>
+        <Carousel.Item>  
     <div><InfoGlacier/></div>
 </Carousel.Item>
-<Carousel.Item>  
-   <div><InfoTemp/></div>
-</Carousel.Item>  
-</Carousel>
-);
-}
+        <Carousel.Item>  
+           <div><InfoTemp/></div>
+        </Carousel.Item>  
+      </Carousel>
+   );
+  }
 }
 
 
   
 
   export default CarouSelCo2; 
+
+  
+  //https://react-bootstrap.github.io/components/carousel/
